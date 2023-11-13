639f50406fc63191ddd03ffcb1a8c21c4df186cd
Igor Degtyarenko <div50015@mail.ru>
2023-11-13 12:50:17 +0300

    Add to delete customer

--- tests/test_xyz_bank.py
+++ tests/test_xyz_bank.py
@@ -5,26 +5,29 @@ from selene import command
 
 def test_add_customer():
     browser.open('/')
-    # уменьшение размера изображения в 0.8 раза
-    # browser.execute_script('document.querySelector(".body-height").style.transform = "scale(.8)"')
-    # ожидание в течение 10 секунд 3х реклпмных банеров и их удаление (вторая строка)
-    # browser.all('[id^google_ads][id$=container__]').with_(timeout=10).wait_until(have.size_less_than_or_equal(3))
-    # browser.all('[id^=google_ads][id$=container__]').perform(command.js.remove)
 
-    browser.element('body > div > div > div.ng-scope > div > div.borderM.box.padT20 > div:nth-child(3) > button').click()
-    browser.element('body > div > div > div.ng - scope > div > div.center > button: nth - child(1)')
+    browser.element('[ng-click="manager()"]').click()
     browser.element('[ng-class=btnClass1]').click()
     browser.element('[ng-model=fName]').type('Piter')
     browser.element('[ng-model=lName]').type('Pen')
     browser.element('[ng-model=postCd]').type('E12345')
-    # time.sleep(3)
     browser.element('[class="btn btn-default"]').click()
-    # time.sleep(3)
     browser.switch_to.alert.accept()
-    # time.sleep(3)
     browser.element('[ng-class="btnClass3"]').click()
-    # time.sleep(1)
-    # browser.element('tr:nth-child(6) td:nth-child(3)').should(have.exact_text('E12345'))
-    browser.all('tr:nth-child(6)>td')[:3].should(have.exact_texts('Piter', 'Pen', 'E12345'))
-    # print(a)
-    # time.sleep(1)
+
+    browser.all('tr').element_by(have.text('Piter')).should(have.text('Piter Pen E12345'))
+    time.sleep(5)
+    browser.element('[ng-click="home()"]').click()
+
+    browser.element('[ng-click="manager()"]').click()
+    time.sleep(5)
+    browser.element('[ng-class="btnClass3"]').click()
+    time.sleep(5)
+    browser.element('[ng-model=searchCustomer]').type('Piter')
+    time.sleep(5)
+    browser.element('[ng-click="deleteCust(cust)"]').click()
+    time.sleep(5)
+
+
+
+
