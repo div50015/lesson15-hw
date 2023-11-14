dd1750fd4cda53b7a670e9610c00117454269c7e
Igor Degtyarenko <div50015@mail.ru>
2023-11-14 22:10:09 +0300

    Add allure-report

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,41 @@
 import pytest
-from selene import browser
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
+from selene import browser
+from utils import attach
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",options=options)
+
+    browser.config.driver = driver
     browser.config.base_url = 'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login'
     browser.config.timeout = 2.0
     browser.config.window_width = 1200
     browser.config.window_height = 1080
 
-#    browser.config.type_by_js = True
+    # browser.config.type_by_js = True
     # закрытие сообщения о том что браузер запущен в отладочном режиме
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_experimental_option("excludeSwitches", ['enable-automation'])
-#    driver_options.add_argument('--headless=new')
-    browser.config.driver_options = driver_options
+    # driver_options = webdriver.ChromeOptions()
+    # driver_options.add_experimental_option("excludeSwitches", ['enable-automation'])
+    # driver_options.add_argument('--headless=new')
+    # browser.config.driver_options = driver_options
 
     yield
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
--- tests/test_xyz_bank.py
+++ tests/test_xyz_bank.py
@@ -2,62 +2,75 @@ import time
 from selene import browser, have, be, query
 import os
 from selene import command
+import allure
 
 def test_add_customer():
-    browser.open('/')
+    allure.dynamic.title('Проверка наличия названия Issue в репозитории')
+    allure.dynamic.tag('web')
+    allure.dynamic.severity(severity_level='Critical')
+    allure.dynamic.feature("Issues")
+    allure.dynamic.story("Тест lambda steps")
+    allure.dynamic.link("https://github.com/", name='Testing')
+    allure.dynamic.label('owner', 'div50015')
 
-    browser.element('[ng-click="manager()"]').click()
-    browser.element('[ng-class=btnClass1]').click()
-    browser.element('[ng-model=fName]').type('Piter')
-    browser.element('[ng-model=lName]').type('Pen')
-    browser.element('[ng-model=postCd]').type('E12345')
-    browser.element('[class="btn btn-default"]').click()
+    with allure.step('Открытие формы'):
+        browser.open('/')
 
-    browser.switch_to.alert.accept()
+    with allure.step('Добавление заказчика'):
+        browser.element('[ng-click="manager()"]').click()
+        browser.element('[ng-class=btnClass1]').click()
+        browser.element('[ng-model=fName]').type('Piter')
+        browser.element('[ng-model=lName]').type('Pen')
+        browser.element('[ng-model=postCd]').type('E12345')
+        browser.element('[class="btn btn-default"]').click()
+        browser.switch_to.alert.accept()
 
-    browser.element('[ng-class="btnClass3"]').click()
+    with allure.step('Проверка добавленя заказчика'):
+        browser.element('[ng-class="btnClass3"]').click()
+        browser.all('tr').element_by(have.text('Piter')).should(have.text('Piter Pen E12345'))
 
-    browser.all('tr').element_by(have.text('Piter')).should(have.text('Piter Pen E12345'))
-
-    browser.element('[ng-click="home()"]').click()
-
-    browser.element('[ng-click="manager()"]').click()
-
-    browser.element('[ng-class="btnClass3"]').click()
-
-    browser.element('[ng-model=searchCustomer]').type('Piter')
-
-    browser.element('[ng-click="deleteCust(cust)"]').click()
+    with allure.step('Удаление заказчика'):
+        browser.element('[ng-click="home()"]').click()
+        browser.element('[ng-click="manager()"]').click()
+        browser.element('[ng-class="btnClass3"]').click()
+        browser.element('[ng-model=searchCustomer]').type('Piter')
+        browser.element('[ng-click="deleteCust(cust)"]').click()
 
 
 def test_deopsit_and_withdrawal():
-    browser.open('/')
 
-    browser.element('[ng-click="customer()"]').click()
-    browser.element('#userSelect').type('Harry Potter')
-    browser.element('[ng-show="custId != \'\'"]').click()
-    browser.all('strong:nth-child(2)').second.should(have.text('0'))
+    with allure.step('Открытие формы'):
+        browser.open('/')
 
-    browser.element('[ng-click="deposit()"]').click()
-    browser.element('input[ng-model=amount]').type('100')
-    browser.element('button[type=submit]').click()
-    browser.element('span[ng-show=message]').should(have.exact_text('Deposit Successful'))
+    with allure.step('Проверка состояния счета'):
+        browser.element('[ng-click="customer()"]').click()
+        browser.element('#userSelect').type('Harry Potter')
+        browser.element('[ng-show="custId != \'\'"]').click()
+        browser.all('strong:nth-child(2)').second.should(have.text('0'))
 
-    browser.element('[ng-click="home()"]').click()
+    with allure.step('Пополнение счета'):
+        browser.element('[ng-click="deposit()"]').click()
+        browser.element('input[ng-model=amount]').type('100')
+        browser.element('button[type=submit]').click()
+        browser.element('span[ng-show=message]').should(have.exact_text('Deposit Successful'))
 
-    browser.element('[ng-click="customer()"]').click()
-    browser.element('#userSelect').type('Harry Potter')
-    browser.element('[ng-show="custId != \'\'"]').click()
-    browser.all('strong:nth-child(2)').second.should(have.text('100'))
+    with allure.step('Проверка состояния счета'):
+        browser.element('[ng-click="home()"]').click()
+        browser.element('[ng-click="customer()"]').click()
+        browser.element('#userSelect').type('Harry Potter')
+        browser.element('[ng-show="custId != \'\'"]').click()
+        browser.all('strong:nth-child(2)').second.should(have.text('100'))
 
-    browser.element('[ng-click="withdrawl()"]').click()
-    browser.element('input[ng-model=amount]').type('70')
-    browser.element('button[type=submit]').click()
-    browser.element('span[ng-show=message]').should(have.exact_text('Transaction successful'))
-    browser.element('[ng-click="home()"]').click()
+    with allure.step('Списание со счета'):
+        browser.element('[ng-click="withdrawl()"]').click()
+        browser.element('input[ng-model=amount]').type('70')
+        browser.element('button[type=submit]').click()
+        browser.element('span[ng-show=message]').should(have.exact_text('Transaction successful'))
+        browser.element('[ng-click="home()"]').click()
 
-    browser.element('[ng-click="customer()"]').click()
-    browser.element('#userSelect').type('Harry Potter')
-    browser.element('[ng-show="custId != \'\'"]').click()
-    browser.all('strong:nth-child(2)').second.should(have.text('30'))
+    with allure.step('Проверка состояния счета'):
+        browser.element('[ng-click="customer()"]').click()
+        browser.element('#userSelect').type('Harry Potter')
+        browser.element('[ng-show="custId != \'\'"]').click()
+        browser.all('strong:nth-child(2)').second.should(have.text('30'))
 
