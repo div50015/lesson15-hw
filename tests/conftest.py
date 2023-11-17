43cceb4d1bf7784c48ccd356a60d0a9ebc78d72a
Igor Degtyarenko <div50015@mail.ru>
2023-11-17 10:17:40 +0300

    Edeit conftest.py file

--- tests/conftest.py
+++ tests/conftest.py
@@ -42,18 +42,11 @@ def browser_management(request):
     driver = webdriver.Remote(command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub", options=options)
 
 
-    # browser.config.driver = driver
+    browser.config.driver = driver
     browser.config.base_url = 'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login'
     browser.config.timeout = 2.0
     browser.config.window_width = 1200
     browser.config.window_height = 1080
-
-    # browser.config.type_by_js = True
-    # закрытие сообщения о том что браузер запущен в отладочном режиме
-    # driver_options = webdriver.ChromeOptions()
-    # driver_options.add_experimental_option("excludeSwitches", ['enable-automation'])
-    # driver_options.add_argument('--headless=new')
-    # browser.config.driver_options = driver_options
 
     yield
 
