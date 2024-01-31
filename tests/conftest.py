c0370b9bc29104f920fb51f3c8329086bd707f59
Igor Degtyarenko <div50015@mail.ru>
2024-01-31 11:47:55 +0300

    Test jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,7 +32,7 @@ def browser_management(request):
         "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
-            "enableVideo": False
+            "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
